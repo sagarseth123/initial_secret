f0d6d08f2808efb067eae7ab84613d5f2f1523bd
sagar seth <sagarseth442@gmail.com>
2020-07-20 16:22:01 +0530

    bc

--- app.js
+++ app.js
@@ -103,7 +103,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets"
+        //callbackURL: "http://localhost:3000/auth/google/secrets"
+        callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
     },
     function(accessToken, refreshToken, profile, cb) {
         //console.log(process.env.CLIENT_ID);
